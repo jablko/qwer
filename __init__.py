dd270e3bfb27ca17870d27ec147de43ff048d284
Jack Bates <ms419@freezone.co.uk>
2011-06-16 18:15:50 -0700

    Support limiting which rules are parenthesized for extracting, to avoid, AssertionError: sorry, but this version only supports 100 named groups

--- __init__.py
+++ __init__.py
@@ -2,102 +2,87 @@ import re, sys, untwisted
 
 __all__ = 'rule', 'qwer'
 
-class asdf:
-  def __init__(ctx, name, *args):
-    ctx.name = name
-    ctx.args = args
-
-  def select(ctx, group, name):
-    group += 1
-    result = []
-
-    if ctx.name == name[0]:
+def select(name, *args):
+  for itm in args:
+    if name[0] == itm[0]:
       if 1 < len(name):
         name = name[1:]
 
       else:
-        result = [(group, ctx)]
+        yield itm[1]
 
-    for itm in ctx.args:
-      group, a = itm.select(group, name)
-      result.extend(a)
-
-    return group, result
+    for itm in select(name, *itm[1][1]):
+      yield itm
 
 class poiu:
   __metaclass__ = type
 
-  def __init__(ctx, match, group, *args):
+  def __init__(ctx, match, *args):
     ctx.match = match
-    ctx.group = group
     ctx.args = args
 
-  class zxcv:
-    class __metaclass__(type):
-      __call__ = lambda ctx, e, name: type.__call__(ctx, e, re.split('\s+', name))
-      __get__ = untwisted.ctxual
+  def mnbv(ctx, e, name):
+    try:
+      name = re.split('\s+', name)
 
-    def __init__(ctx, e, name):
-      ctx.name = name
+    except TypeError:
+      return poiu(ctx.match, ctx.args[name])
 
-      ctx.iuyta = []
+    a = []
+    for itm in ctx.args:
+      a.extend(select(name, *itm[1]))
 
-      group = ctx.ctx.group
-      for itm in ctx.ctx.args:
-        group, a = itm.select(group, name)
-        ctx.iuyta.extend(a)
+    if not a:
+      raise e
 
-      if not ctx.iuyta:
-        raise e
+    return poiu(ctx.match, *filter(lambda itm: ctx.match.group(itm[0]), a))
 
-    def mnbv(ctx, e, name):
-      try:
-        name = re.split('\s+', name)
+  __getattr__ = untwisted.partial(mnbv, AttributeError)
+  __getitem__ = untwisted.partial(mnbv, KeyError)
 
-      except TypeError:
-        try:
-          return ctx.iuytb[name]
-
-        except AttributeError:
-          ctx.iuytb = [poiu(ctx.ctx.match, group, *itm.args) for group, itm in ctx.iuyta if ctx.ctx.match.group(group)]
-
-          return ctx.iuytb[name]
-
-      return type.__call__(ctx.ctx.zxcv, e, ctx.name + name)
-
-    __getattr__ = untwisted.partial(mnbv, AttributeError)
-    __getitem__ = untwisted.partial(mnbv, KeyError)
-
-    __int__ = lambda ctx: int(ctx[0])
-    __str__ = lambda ctx: str(ctx[0])
-
-  __getattr__ = untwisted.partial(zxcv, AttributeError)
-  __getitem__ = untwisted.partial(zxcv, KeyError)
-
-  __int__ = lambda ctx: int(ctx.match.group(ctx.group))
-  __len__ = lambda ctx: ctx.match.end(ctx.group) - ctx.match.start(ctx.group)
-  __str__ = lambda ctx: ctx.match.group(ctx.group)
+  __int__ = lambda ctx: int(ctx.match.group(ctx.args[0][0]))
+  __len__ = lambda ctx: ctx.match.end(ctx.args[0][0]) - ctx.match.start(ctx.args[0][0])
+  __str__ = lambda ctx: ctx.match.group(ctx.args[0][0])
 
 class rule:
   def __init__(ctx, name):
     ctx.namespace = sys._getframe().f_back.f_locals
     ctx.name = name.split('.')
 
-  def compile(ctx):
-    pattern, b = reduce(getattr, ctx.name[1:], ctx.namespace[ctx.name[0]]).compile()
+  def compile(ctx, *args):
+    qwer = reduce(getattr, ctx.name[1:], ctx.namespace[ctx.name[0]])
 
-    return '(' + pattern + ')', (asdf(ctx.name[-1], *b),)
+    a = False
+    b = list(args[:1])
+    for itm in args[1:]:
+      if ctx.name[-1] == itm[0]:
+        if 1 < len(itm):
+          itm = itm[1:]
+
+        else:
+          a = True
+
+      b.append(itm)
+
+    pattern, b = qwer.compile(*b)
+    if a:
+      c = args[0][-1][0] + 1, b
+      args[0].append(c)
+
+      return '(' + pattern + ')', ((ctx.name[-1], c),)
+
+    return pattern, b
 
 class qwer:
   def __init__(ctx, *args):
     ctx.args = args
 
-  def compile(ctx):
+  def compile(ctx, *args):
     pattern = []
     b = []
     for itm in ctx.args:
       try:
-        itm, c = itm.compile()
+        itm, c = itm.compile(*args)
 
       except AttributeError:
         pass
@@ -109,14 +94,17 @@ class qwer:
 
     return ''.join(pattern), b
 
-  def lkjh(ctx, jhgf, subject):
-    pattern, b = ctx.compile()
+  def lkjh(ctx, jhgf, subject, *args):
+    a = [(0, [])]
+    pattern, _ = ctx.compile(a, *map(untwisted.partial(re.split, '\s+'), args))
+    if args:
+      a.pop(0)
 
     match = jhgf(pattern, subject)
     if not match:
       raise ValueError
 
-    return poiu(match, 0, *b)
+    return poiu(match, *filter(lambda itm: match.group(itm[0]), a))
 
   match = untwisted.partial(lkjh, re.match)
   search = untwisted.partial(lkjh, re.search)
